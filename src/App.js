4e7f16719aac46c7624c1a0adf115626df876e61
Orion Wong <orion@Orions-MacBook-Pro.local>
2021-01-01 17:05:50 +0800

    🐛 i edited again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hellooooo
+          Edit <code>src/App.js</code> and save to reload. Heyyyyy
         </p>
         <a
           className="App-link"
